911021fc894ab704bd5ca0bbbf595ed8c67de204
lexangross51 <lexangross51@gmail.com>
2022-10-25 14:04:09 +0700

    Добавил класс вектора

---
+++ Diploma/Source/MathClasses/Vector.cs
@@ -0,0 +1,120 @@
+﻿namespace Diploma.Source.MathClasses;
+
+public class Vector : IEnumerable<double>
+{
+    private readonly double[] _storage;
+    public int Length { get; }
+
+    public double this[int idx]
+    {
+        get => _storage[idx];
+        set => _storage[idx] = value;
+    }
+
+    public Vector(int length)
+        => (Length, _storage) = (length, new double[length]);
+
+    public static double operator *(Vector a, Vector b)
+    {
+        double result = 0.0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            result += a[i] * b[i];
+        }
+
+        return result;
+    }
+
+    public static Vector operator *(double constant, Vector vector)
+    {
+        Vector result = new(vector.Length);
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result[i] = vector[i] * constant;
+        }
+
+        return result;
+    }
+
+    public static Vector operator +(Vector a, Vector b)
+    {
+        Vector result = new(a.Length);
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] + b[i];
+        }
+
+        return result;
+    }
+
+    public static Vector operator -(Vector a, Vector b)
+    {
+        Vector result = new(a.Length);
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] - b[i];
+        }
+
+        return result;
+    }
+
+    public static void Copy(Vector source, Vector destination)
+    {
+        for (int i = 0; i < source.Length; i++)
+        {
+            destination[i] = source[i];
+        }
+    }
+
+    public void Fill(double value)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            _storage[i] = value;
+        }
+    }
+
+    public double Norm()
+    {
+        double result = 0.0;
+
+        for (int i = 0; i < Length; i++)
+        {
+            result += _storage[i] * _storage[i];
+        }
+
+        return Math.Sqrt(Convert.ToDouble(result));
+    }
+
+    public ImmutableArray<double> ToImmutableArray()
+        => ImmutableArray.Create(_storage);
+
+    public IEnumerator<double> GetEnumerator()
+    {
+        foreach (var value in _storage)
+        {
+            yield return value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void Add(IEnumerable<double> collection)
+    {
+        var enumerable = collection as double[] ?? collection.ToArray();
+
+        if (Length != enumerable.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(collection), "Sizes of vector and collection not equal");
+        }
+
+        for (int i = 0; i < Length; i++)
+        {
+            _storage[i] = enumerable[i];
+        }
+    }
+}
