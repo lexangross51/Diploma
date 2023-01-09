f0ac9aa46d62a324ae1f36e7592896c561e128fd
lexan <lexangross51@gmail.com>
2023-01-09 23:16:09 +0700

    Добавил переменную, которая говорит о том, фиктивный это элемент или нет(нужен для того, чтобы пропускать скважинные элементы)

--- Diploma/Source/Mesh/FiniteElement.cs
+++ Diploma/Source/Mesh/FiniteElement.cs
@@ -9,11 +9,13 @@ public class FiniteElement
     public List<Edge> Edges { get; }
     public List<int> EdgesDirect { get; set; }
     public int Area { get; set; }
+    public bool IsFictitious { get; set; }
 
     public FiniteElement(int[] nodes, int area)
     {
         Nodes = nodes.ToList();
         Area = area;
+        IsFictitious = false;
         EdgesIndices = new List<int>(4);
         Edges = new List<Edge>(4);
         EdgesDirect = new List<int>(4);
